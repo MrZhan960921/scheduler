d8b9e0b1af2d292fcb5b8924231d7d45be31d84b
zcq <zcq@zcqdeMacBook-Pro.local>
2019-11-25 23:12:59 +0800

    Cron触发器

--- src/main/java/com/zcq/QuartzDemo.java
+++ src/main/java/com/zcq/QuartzDemo.java
@@ -24,10 +24,9 @@ public class QuartzDemo {
                 .usingJobData("msg","trigger1")
                 //设置优先级，只有在同等时间且线程不够的情况下才会触发优先级
                 .withPriority(1)
-                .withSchedule(SimpleScheduleBuilder.simpleSchedule()
-                    .withIntervalInSeconds(5)
-                        .withRepeatCount(5)
-                ).endAt(DateBuilder.futureDate(20,DateBuilder.IntervalUnit.SECOND))
+                //cron表达式周和月内每一天有冲突，有一个要用？表示什么都不匹配防止冲突
+                .withSchedule(CronScheduleBuilder.cronSchedule("12/5 * * * * ?")
+                )
                 .build();
 
 
