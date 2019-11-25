9319957500b7bd08b47a0b6a6326fb8c9a7d29c3
zcq <zcq@zcqdeMacBook-Pro.local>
2019-11-25 22:55:45 +0800

    Simple触发器

--- src/main/java/com/zcq/QuartzDemo.java
+++ src/main/java/com/zcq/QuartzDemo.java
@@ -16,9 +16,7 @@ public class QuartzDemo {
                 .withIdentity("jobDetail1","group1")
                 .build();
 
-        JobDetail jobDetail2=JobBuilder.newJob(MyJob.class)
-                .withIdentity("jobDetail2","group1")
-                .build();
+
 
         Date date=DateBuilder.futureDate(5,DateBuilder.IntervalUnit.SECOND);
         Trigger trigger1=TriggerBuilder.newTrigger()
@@ -28,20 +26,14 @@ public class QuartzDemo {
                 .withPriority(1)
                 .withSchedule(SimpleScheduleBuilder.simpleSchedule()
                     .withIntervalInSeconds(5)
-                        .repeatForever()
-                ).build();
+                        .withRepeatCount(5)
+                ).endAt(DateBuilder.futureDate(20,DateBuilder.IntervalUnit.SECOND))
+                .build();
 
-        Trigger trigger2=TriggerBuilder.newTrigger()
-                .startAt(date)
-                .usingJobData("msg","trigger2")
-                .withPriority(2)
-                .withSchedule(SimpleScheduleBuilder.simpleSchedule()
-                        .withIntervalInSeconds(5)
-                        .repeatForever()
-                ).build();
+
         //一个jobDetail只能对应一个trigger
         scheduler.scheduleJob(jobDetail1,trigger1);
-        scheduler.scheduleJob(jobDetail2,trigger2);
+
         try {
             Thread.sleep(600000);
         } catch (InterruptedException e) {
