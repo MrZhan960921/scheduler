87427b467dd07a395b053695d89b52574ed5824c
zcq <zcq@zcqdeMacBook-Pro.local>
2019-11-25 22:50:02 +0800

    Trigger优先级

--- src/main/java/com/zcq/QuartzDemo.java
+++ src/main/java/com/zcq/QuartzDemo.java
@@ -4,27 +4,44 @@ import com.zcq.job.MyJob;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
+import java.util.Date;
 import java.util.Random;
 
 public class QuartzDemo {
     public static void main(String[] args) throws SchedulerException {
         Scheduler scheduler=StdSchedulerFactory.getDefaultScheduler();
         scheduler.start();
-        Random random=new Random();
-        int count=random.nextInt(10);
-        JobDetail jobDetail=JobBuilder.newJob(MyJob.class)
+
+        JobDetail jobDetail1=JobBuilder.newJob(MyJob.class)
                 .withIdentity("jobDetail1","group1")
-                .usingJobData("count",count)
                 .build();
 
-        Trigger trigger=TriggerBuilder.newTrigger()
-                .startNow()
+        JobDetail jobDetail2=JobBuilder.newJob(MyJob.class)
+                .withIdentity("jobDetail2","group1")
+                .build();
+
+        Date date=DateBuilder.futureDate(5,DateBuilder.IntervalUnit.SECOND);
+        Trigger trigger1=TriggerBuilder.newTrigger()
+                .startAt(date)
+                .usingJobData("msg","trigger1")
+                //设置优先级，只有在同等时间且线程不够的情况下才会触发优先级
+                .withPriority(1)
                 .withSchedule(SimpleScheduleBuilder.simpleSchedule()
                     .withIntervalInSeconds(5)
                         .repeatForever()
                 ).build();
 
-        scheduler.scheduleJob(jobDetail,trigger);
+        Trigger trigger2=TriggerBuilder.newTrigger()
+                .startAt(date)
+                .usingJobData("msg","trigger2")
+                .withPriority(2)
+                .withSchedule(SimpleScheduleBuilder.simpleSchedule()
+                        .withIntervalInSeconds(5)
+                        .repeatForever()
+                ).build();
+        //一个jobDetail只能对应一个trigger
+        scheduler.scheduleJob(jobDetail1,trigger1);
+        scheduler.scheduleJob(jobDetail2,trigger2);
         try {
             Thread.sleep(600000);
         } catch (InterruptedException e) {
--- src/main/java/com/zcq/job/MyJob.java
+++ src/main/java/com/zcq/job/MyJob.java
@@ -1,5 +1,7 @@
 package com.zcq.job;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.quartz.*;
 
 import java.time.LocalTime;
@@ -8,27 +10,16 @@ import java.time.LocalTime;
  * @author Maybeeeee
  * @date 2019/11/25 9:06 下午
  */
-@PersistJobDataAfterExecution
+//@PersistJobDataAfterExecution
 public class MyJob implements Job {
-    private String count;
-
-    public String getCount() {
-        return count;
-    }
-
-    public void setCount(String count) {
-        this.count = count;
-    }
+    @Setter
+    @Getter
+    private String msg;
 
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        //job每次执行都会创建一个新的对象，所以不要引用外部变量来操作传递
-        //jobExecutionContext可以获取jobdetail和 trigger等信息
-//        String name = jobExecutionContext.getJobDetail().getJobDataMap().getString("name");
-        int count= (int) jobExecutionContext.getJobDetail().getJobDataMap().get("count");
-        count++;
-        jobExecutionContext.getJobDetail().getJobDataMap().put("count",count);
+
         LocalTime localTime = LocalTime.now();
-        System.out.println(count);
+        System.out.println(localTime.toString()+"，msg="+msg);
 
     }
 }
