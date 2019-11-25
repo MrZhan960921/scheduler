c07334b5f54a93ed6afb63e6306a70cb9afa05e9
zcq <zcq@zcqdeMacBook-Pro.local>
2019-11-25 22:25:35 +0800

    @PersistJobDataAfterExecution注解更新JobDataMap

--- src/main/java/com/zcq/QuartzDemo.java
+++ src/main/java/com/zcq/QuartzDemo.java
@@ -4,13 +4,17 @@ import com.zcq.job.MyJob;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
+import java.util.Random;
+
 public class QuartzDemo {
     public static void main(String[] args) throws SchedulerException {
         Scheduler scheduler=StdSchedulerFactory.getDefaultScheduler();
         scheduler.start();
+        Random random=new Random();
+        int count=random.nextInt(10);
         JobDetail jobDetail=JobBuilder.newJob(MyJob.class)
                 .withIdentity("jobDetail1","group1")
-                .usingJobData("name","james")
+                .usingJobData("count",count)
                 .build();
 
         Trigger trigger=TriggerBuilder.newTrigger()
--- src/main/java/com/zcq/job/MyJob.java
+++ src/main/java/com/zcq/job/MyJob.java
@@ -1,9 +1,6 @@
 package com.zcq.job;
 
-import org.quartz.DisallowConcurrentExecution;
-import org.quartz.Job;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
+import org.quartz.*;
 
 import java.time.LocalTime;
 
@@ -11,30 +8,27 @@ import java.time.LocalTime;
  * @author Maybeeeee
  * @date 2019/11/25 9:06 下午
  */
-@DisallowConcurrentExecution
+@PersistJobDataAfterExecution
 public class MyJob implements Job {
-    private String name;
+    private String count;
 
-    public String getName() {
-        return name;
+    public String getCount() {
+        return count;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setCount(String count) {
+        this.count = count;
     }
 
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         //job每次执行都会创建一个新的对象，所以不要引用外部变量来操作传递
         //jobExecutionContext可以获取jobdetail和 trigger等信息
 //        String name = jobExecutionContext.getJobDetail().getJobDataMap().getString("name");
+        int count= (int) jobExecutionContext.getJobDetail().getJobDataMap().get("count");
+        count++;
+        jobExecutionContext.getJobDetail().getJobDataMap().put("count",count);
         LocalTime localTime = LocalTime.now();
-        System.out.println(Thread.currentThread().getName() + "开始执行" + localTime.toString());
-        try {
-            Thread.sleep(7000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        LocalTime endTime = LocalTime.now();
-        System.out.println(Thread.currentThread().getName() + "结束执行" + endTime.toString());
+        System.out.println(count);
+
     }
 }
